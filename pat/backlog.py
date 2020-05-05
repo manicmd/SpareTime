224a34ca61972b33d8028a2cb2f49c94abb67ea2
mandjiny <mandjiny89@gmail.com>
2020-05-05 16:47:32 +0100

    list function done

---
+++ pat/backlog.py
@@ -0,0 +1,17 @@
+fruit = ["apple", "banana", "mango"]
+veg = ['carrot', 'beans', 'potato']
+drink = ['milk', 'water', 'juice']
+
+
+def what_is_the_item(item):
+    """ Write a function here that print what kind of item is given from the above lists.
+    Example: if 'apple' given then it should print 'Its a Fruit' """
+
+
+def merge_list(list1, list2, list3):
+    """Write a function which accepts 3 lists and return a new list called 'food' with
+    combined all items from given lists."""
+
+
+
+
