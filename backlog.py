9e60295e138aa2f2b3803ac8d3a6a6ea7bff13c4
intelliflovrk <raj.veeramani@intelliflo.com>
2020-05-05 19:03:19 +0100

    backlog updated

--- backlog.py
+++ backlog.py
@@ -2,15 +2,21 @@ fruit = ["apple", "banana", "mango"]
 veg = ['carrot', 'beans', 'potato']
 drink = ['milk', 'water', 'juice']
 
-
+#T1
 def what_is_the_item(item):
     """ Write a function here that print what kind of item is given from the above lists.
     Example: if 'apple' given then it should print 'Its a Fruit' """
 
-
+#T2
 def merge_list(list1, list2, list3):
     """Write a function which accepts 3 lists and return a new list called 'food' with
     combined all items from given lists."""
+
+#T3
+"""Create a function which accepts a params, and returns key name where the value(params) is present 
+by parsing the json file(food.json). Please find the attached sample json file."""
+
+
 
 
 
