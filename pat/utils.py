869c6924323707db4ef162e5f8a520e184a89003
mandjiny <mandjiny89@gmail.com>
2020-05-05 16:55:04 +0100

    updating list
    updating the list script

--- pat/utils.py
+++ pat/utils.py
@@ -1,14 +1,17 @@
-print("########################################")
+#Below statements will print user friendly list to choose
+print("")
 print("Select a fruit 'apple','banana','mango' or ")
 print("Select a drink 'milk', 'water', 'juice' or ")
 print("Select a veg 'carrot','beans','potato' ")
-print("########################################")
+print("")
+
 user_need = input("What do you like from the above list: ")
 
 fruits = ["apple", "banana", "mango"]
 vegs = ['carrot', 'beans', 'potato']
 drinks = ['milk', 'water', 'juice']
 
+# Below what_is_the_item function will retun is the entered item is what list.
 def what_is_the_item(item):
     if item in fruits:
         print(item + " It's a fruit")
@@ -17,16 +20,15 @@ def what_is_the_item(item):
     elif item in vegs:
         print(item + " It's a veg")
     else:
-        print("Choose the wrong option from the list")
-
-what_is_the_item(user_need)
+        print("Chosen the wrong option from the list given")
 
 
+# Below merge_list function will print all the given list into a single list
 def merge_list(*no_of_list):
 #    full_list = list1 + list2 + list3
     full_list1 = no_of_list[0] + no_of_list[1] + no_of_list[2]
     print("This is the full list: ", full_list1)
 
+# Functions will be called in this section
 merge_list(fruits, vegs, drinks)
-
-
+what_is_the_item(user_need)
